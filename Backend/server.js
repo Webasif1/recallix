1ec124393066eebd6ca8c568187618c2d77aedf9
Webasif1 <mdasif606392@gmail.com>
2026-03-25 21:51:54 +0600

    update code

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,5 @@
-import app from "./src/app.js"
+import app from "./src/app.js";
 
-
-app.listen(3000, ()=>{
+app.listen(3000, () => {
   console.log(`The server is running on port 3000`);
-})
+});
--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,9 @@
-import express from "express"
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get("/", (req,res)=>{
-  res.send("Welcome")
-})
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 
-export default app
+export default app;
