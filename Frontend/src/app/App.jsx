81ee448ab62574195334456ec0f69e8fc504f630
Webasif1 <mdasif606392@gmail.com>
2026-03-29 16:08:22 +0600

    basic frontend setup and redux store set up and auth api create

---
+++ Frontend/src/app/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-3xl">Frontend Setup Done 🚀</h1>
+    </div>
+  )
+}
+
+export default App
