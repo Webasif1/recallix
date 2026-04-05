51e167cf26628022f503781e8fc51d6c731e70db
Webasif1 <mdasif606392@gmail.com>
2026-04-05 15:29:25 +0600

    app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,13 +4,9 @@ import itemRoutes from "./routes/item.route.js";
 import morgan from "morgan";
 import cookie from "cookie-parser";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
+
 
 const app = express();
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Middleware
 app.use(express.json());
@@ -23,20 +19,16 @@ app.use(
       "chrome-extension://oanhpfjblpgdfnnfgkfjhakcchnmjhfc",
       "https://recallix-3xvg.onrender.com",
       "https://69d224a1c1d67ef51d7855d3--recallixgg.netlify.app",
-      "http://localhost:5173",
+      "http://localhost:5173"
     ], // your extension ID
     credentials: true,
     methods: ["GET", "POST", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-app.use(express.static(path.join(__dirname, "dist")));
 
 app.get("/", (req, res) => {
   res.send("Welcome");
-});
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
 app.use("/api/auth", authRouter);
