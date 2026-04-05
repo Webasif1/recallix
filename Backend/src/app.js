3291938a0941a4b809562bda54c0b001755fc4d6
Webasif1 <mdasif606392@gmail.com>
2026-04-05 20:18:13 +0600

    remove catch-all route and unused path import

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -28,11 +28,6 @@ app.use(
   }),
 );
 
-const path = require("path");
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
 app.get("/", (req, res) => {
   res.send("Welcome");
 });
