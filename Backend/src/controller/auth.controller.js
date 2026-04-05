a7301352b700356702cfce8db260c24ce4b9529f
Webasif1 <mdasif606392@gmail.com>
2026-04-05 16:29:35 +0600

    fixing cookie problem

--- Backend/src/controller/auth.controller.js
+++ Backend/src/controller/auth.controller.js
@@ -82,7 +82,7 @@ export async function login(req, res) {
 
   res.cookie("token", token, {
     domain: "https://recallix.onrender.com/",
-    httpOnly: false,
+    httpOnly: true,
     secure: true,
     sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 * 1000,
