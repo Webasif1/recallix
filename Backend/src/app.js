fcb51068645fc55342ff17cbdb6c8e60953908cc
Webasif1 <mdasif606392@gmail.com>
2026-04-05 14:08:58 +0600

    setup cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "chrome-extension://oanhpfjblpgdfnnfgkfjhakcchnmjhfc",
-      "https://recallix-3xvg.onrender.com/",
+      "https://recallix-3xvg.onrender.com",
     ], // your extension ID
     credentials: true,
     methods: ["GET", "POST",, "DELETE", "OPTIONS"],
