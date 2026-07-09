de20ad05a1e8135713b1f5eabe99b21aabd66cc1
Webasif1 <mdasif606392@gmail.com>
2026-07-09 23:09:41 +0600

    add error handling to MongoDB connection

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -1,8 +1,12 @@
 import mongoose from "mongoose";
 
 async function connectToDb() {
-  const conn = await mongoose.connect(process.env.MONGO_URI);
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
   console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`error in mongodb-${error}`)
+  }
 }
 
 export default connectToDb;
